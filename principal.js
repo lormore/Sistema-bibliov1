2ece59917a4c0a231d30ad344b1d6da9c68b1f8f
lormore <loreannemoreno@hotmail.com>
2020-04-02 19:19:26 -0400

    Primer commit

---
+++ principal.js
@@ -0,0 +1,24 @@
+let slider = document.querySelector('.slider-contenedor');
+let sliderInd = document.querySelectorAll('.slider-test');
+let contador = 1;
+let tamañoWidth = sliderInd[0].clientWidth;
+let intervalo= 3000;
+
+setInterval(function tiempo(){
+slides();
+}, intervalo);
+
+function slides(){
+    slider.style.transform = 'translate('+ (- tamañoWidth * contador) + 'px)';
+    slider.style.transition = 'transform 1s';
+    contador++;
+
+    if(contador=== sliderInd.length){
+        contador=0;
+
+        setTimeout(function(){
+            slider.style.transform = 'translate(0px)';
+            slider.style.transition = 'transform 0s';
+        }, intervalo)
+    }
+}
